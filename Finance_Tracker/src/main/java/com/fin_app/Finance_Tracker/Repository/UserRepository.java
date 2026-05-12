1aac88773eb777a82491df8d52106635f5849908
aman <amanpappara@gmail.com>
2026-05-12 11:40:21 +0530

    repo_change

--- Finance_Tracker/src/main/java/com/fin_app/Finance_Tracker/Repository/UserRepository.java
+++ Finance_Tracker/src/main/java/com/fin_app/Finance_Tracker/Repository/UserRepository.java
@@ -4,6 +4,6 @@ import com.fin_app.Finance_Tracker.Entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    User findByuserEmail(String reqEmail) ;
+    User findByUserEmail(String reqEmail) ;
 //    User getByUser(User user);
 }
