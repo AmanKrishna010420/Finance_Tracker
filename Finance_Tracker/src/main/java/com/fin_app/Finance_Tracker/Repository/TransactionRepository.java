09047f7bab171676dccc83810af5a50d05a63c24
aman <amanpappara@gmail.com>
2026-05-12 15:23:46 +0530

    RepoImport

--- Finance_Tracker/src/main/java/com/fin_app/Finance_Tracker/Repository/TransactionRepository.java
+++ Finance_Tracker/src/main/java/com/fin_app/Finance_Tracker/Repository/TransactionRepository.java
@@ -3,6 +3,8 @@ package com.fin_app.Finance_Tracker.Repository;
 import com.fin_app.Finance_Tracker.Entities.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
 
 }
